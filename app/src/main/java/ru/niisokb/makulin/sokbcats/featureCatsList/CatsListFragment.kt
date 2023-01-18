d200e3535ff39658634dbb9ec6c84c3f57dd3c55
PavelMakulin <p.a.makulin@gmail.com>
2023-01-18 12:58:54 +0300

    Добавил начальную реализацию навигации. Сделал переход на стартовый пустой фрагмент.

---
+++ app/src/main/java/ru/niisokb/makulin/sokbcats/featureCatsList/CatsListFragment.kt
@@ -0,0 +1,7 @@
+package ru.niisokb.makulin.sokbcats.featureCatsList
+
+import androidx.fragment.app.Fragment
+import ru.niisokb.makulin.sokbcats.R
+
+class CatsListFragment : Fragment(R.layout.fragment_catslist) {
+}
