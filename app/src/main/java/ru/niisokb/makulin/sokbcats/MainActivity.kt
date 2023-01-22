62b3e5a4cc86df5af9f869d78833f2f98f42e0fc
PavelMakulin <p.a.makulin@gmail.com>
2023-01-23 01:25:50 +0300

    Добавил меню и навигацию через него.

--- app/src/main/java/ru/niisokb/makulin/sokbcats/MainActivity.kt
+++ app/src/main/java/ru/niisokb/makulin/sokbcats/MainActivity.kt
@@ -1,11 +1,27 @@
 package ru.niisokb.makulin.sokbcats
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
