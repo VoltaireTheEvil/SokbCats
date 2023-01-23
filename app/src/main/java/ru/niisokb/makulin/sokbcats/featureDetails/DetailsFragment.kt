98d06deec14da0d6624bcb35b2f0a2240804c02b
PavelMakulin <p.a.makulin@gmail.com>
2023-01-23 03:40:43 +0300

    Создал макет для DetailsFragment.kt

---
+++ app/src/main/java/ru/niisokb/makulin/sokbcats/featureDetails/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package ru.niisokb.makulin.sokbcats.featureDetails
+
+import androidx.fragment.app.Fragment
+import ru.niisokb.makulin.sokbcats.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+}
